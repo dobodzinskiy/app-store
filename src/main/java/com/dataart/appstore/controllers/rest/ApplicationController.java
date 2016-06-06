b9b2c2127c33f7b0c44bb947fc86f51a6181dcc2
dobodzinskiy <denisobodzinskiy@gmail.com>
2016-06-06 19:38:18 +0300

    working on uploading. ajax + multipart

--- src/main/java/com/dataart/appstore/controllers/rest/ApplicationController.java
+++ src/main/java/com/dataart/appstore/controllers/rest/ApplicationController.java
@@ -5,12 +5,15 @@ import com.dataart.appstore.dto.UploadApplicationDto;
 import com.dataart.appstore.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -30,6 +33,12 @@ public class ApplicationController {
     @ResponseStatus(HttpStatus.OK)
     public ApplicationDto uploadApplication(@RequestBody UploadApplicationDto uploadApplicationDto) {
         return applicationService.uploadApplication(uploadApplicationDto);
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile multipartFile) {
+        String name = multipartFile.getName();
+        return new ResponseEntity<Object>(name, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
--- src/main/webapp/react/index.js
+++ src/main/webapp/react/index.js
@@ -4,36 +4,60 @@ import $ from 'jquery';
 
 class App extends React.Component {
     handleSubmit(e) {
+        //var application = {
+        //    name: 'app',
+        //    archive: $("#archive").file,
+        //    applicationType: 'Art',
+        //    description: 'application'
+        //};
         e.preventDefault();
-        var application = {
-            name : 'app',
-            archive: $("#archive"),
-            applicationType: 'Art',
-            description: 'application'
-        };
+        var data = new FormData();
+        data.append("file", $('input[type=file]')[0].files[0]);
         $.ajax({
-            url: '/applications',
+            url: '/applications/upload',
             type: 'POST',
-            data: JSON.stringify(application),
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Accept", "application/json");
-                xhr.setRequestHeader("Content-Type", "application/json");
-            },
+            data: data,
+            enctype: 'multipart/form-data',
+            processData: false,
+            contentType:false,
             success: function () {
                 alert("done");
             }
-        })
+        });
     }
     render() {
         return (
             <div class="container">
                 <form id="form-file" role="form" onSubmit={this.handleSubmit}>
-                    <input type="file" class="form-control" id="archive" />
+                    <div class="form-group">
+                        <label for="archive">Archive</label>
+                        <input type="file" class="form-control" id="archive"/>
+                    </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
+
+
             </div>
         )
     }
 }
-
+//<form id="form-file" role="form" onSubmit={this.handleSubmit}>
+//    <div class="form-group">
+//        <label for="name">Name</label>
+//        <input type="text" class="form-control" id="name"/>
+//    </div>
+//    <div class="form-group">
+//        <label for="type">Type</label>
+//        <input type="text" class="form-control" id="type"/>
+//    </div>
+//    <div class="form-group">
+//        <label for="archive">Archive</label>
+//        <input type="file" class="form-control" id="archive"/>
+//    </div>
+//    <div class="form-group">
+//        <label for="description">Description</label>
+//        <input type="text" class="form-control" id="description"/>
+//    </div>
+//    <button type="submit" class="btn btn-primary">Submit</button>
+//</form>
 ReactDOM.render(<App />, document.getElementById("app"));
